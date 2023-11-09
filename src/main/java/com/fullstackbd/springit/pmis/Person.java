25ea2f145658d7aa61494235cb82f7df04378ba3
fullstackltd <fullstackltd@gmail.com>
2023-11-09 20:19:44 +0600

    pmis > Person add

---
+++ src/main/java/com/fullstackbd/springit/pmis/Person.java
@@ -0,0 +1,32 @@
+package com.fullstackbd.springit.pmis;
+
+public class Person {
+
+  private Long id;
+  private String name;
+  private String email;
+
+  public Person() {
+  }
+
+  public Person(Long id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  public Long getId() { return id; }
+  public void setId(Long id) { this.id = id; }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+  public String getEmail() { return email; }
+  public void setEmail(String email) { this.email = email; }
+  public String toString() {
+    return "Person{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", email='" + email + '\'' +
+      '}';
+  }
+  
+}
